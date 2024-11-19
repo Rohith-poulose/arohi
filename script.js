59486804fe783aeb0f800cfe789b3645c4d4b332
Rohith P <rohithinteractive14@gmail.com>
2024-11-19 23:10:46 +0530

    feat google form

--- script.js
+++ script.js
@@ -191,18 +191,17 @@ function showtogether(){
 
 }
 
-// const scriptURL = 'https://script.google.com/macros/s/AKfycbzl15JFDIfBJv3Iez2ReS_9ql_bIKbeIE7PJERblO70rDTWFKXkR7aVrSwuS-eUUtqp/exec'
-// const form = document.forms['submit-to-google-sheet']
-// const msg = document.getElementById("msg")
-// form.addEventListener('submit', e => {
-//   e.preventDefault()
-//   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-//     .then(response => {
-//         msg.innerHTML="Message sent successfully"
-//         setTimeout (function(){
-//             msg.innerHTML="";
-//         },5000);
-//         form.reset()
-//     })
-//     .catch(error => console.error('Error!', error.message))
-// })
+const scriptURL = 'https://script.google.com/macros/s/AKfycbx3Xe6juZVPddRr1peW5vtqAzBv9dJRriUyJbICgYhfgI1CAXMWk8Lo-0LGHR29GPsQ/exec'
+const form = document.forms['submit-to-google-sheet']
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        msg.innerHTML="Message sent successfully"
+        setTimeout (function(){
+            msg.innerHTML="";
+        },5000);
+        form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
